802e56906c543a3e889e78a31625acf4da54d25a
Daniel <d.spinardi199@gmail.com>
2021-03-19 13:27:55 -0300

    Add component example

---
+++ author/package/component.blade.php
@@ -0,0 +1,13 @@
+<section>
+    {{ $section_text }}
+    <img
+        src="{{ $section_img }}"
+        alt="{{ $section_img_alt }}"
+    >
+</section>
+
+@push('style')
+    <style>
+        /* user custom css */
+    </style>
+@endpush
